e80ccdc50adffc2fee2634f7d0f003fb0119789f
sully34 <sssulai@gmail.com>
2021-07-15 18:25:42 -0400

    added vuetify and views

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,41 +1,49 @@
 <template>
-  <v-app>
-    <v-navigation-drawer app></v-navigation-drawer>
-    <v-content>
-      <div>
-        <p>
-          Ambitious, user-focused Web and Software Developer with experience
-          working in teams and independently. Has strong project management
-          experience and experience delegating responsibilities to a team from
-          time at Maximus where he spearheaded a project to create the audio/ADA
-          compliant portion of a new healthcare plan for 1.5 million citizens of
-          New York State.te sapiente corrupti asperiores rem nemo numquam fuga
-          ab at.
-        </p>
-        <p>
-          A flexible and quick learner committed to constantly learning learning
-          and evolving his skills to stay abreast of ever-evolving technologies
-          and trends in the industry. Communicative, knowledgeable and steadfast
-          with excellent computer engineering skills learned in the Columbia
-          University Coding Bootcamp where he created projects in HTML5, CSS,
-          JavaScript, VueJS, and ReactJS.
-        </p>
-      </div>
-      <v-container fluid>
-        <router-view></router-view>
-      </v-container>
-    </v-content>
-    <v-footer app></v-footer>
-  </v-app>
+  <v-layout>
+    <v-flex xs12 sm6 offset-sm3>
+      <v-card>
+        <v-img
+          src="https://cdn.vuetifyjs.com/images/cards/desert.jpg"
+          aspect-ratio="2.75"
+        ></v-img>
+
+        <v-card-title primary-title>
+          <div>
+            <h3 class="headline mb-0">
+              Ambitious, user-focused Web and Software Developer with experience
+              working in teams and independently. Has strong project management
+              experience and experience delegating responsibilities to a team
+              from time at Maximus where he spearheaded a project to create the
+              audio/ADA compliant portion of a new healthcare plan for 1.5
+              million citizens of New York State.
+            </h3>
+            <div></div>
+          </div>
+        </v-card-title>
+        <div>
+          <h4 class="headline mb-0">
+            A flexible and quick learner committed to constantly learning
+            learning and evolving his skills to stay abreast of ever-evolving
+            technologies and trends in the industry. Communicative,
+            knowledgeable and steadfast with excellent computer engineering
+            skills learned in the Columbia University Coding Bootcamp where he
+            created projects in HTML5, CSS, JavaScript, VueJS, and ReactJS.
+          </h4>
+          <div></div>
+        </div>
+        <v-card-actions>
+          <v-btn flat color="orange">Share</v-btn>
+          <v-btn flat color="orange">Explore</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
 export default {
-  data: () => ({
-    drawer: null,
-  }),
-  props: {
-    source: String,
+  data() {
+    return {};
   },
 };
 </script>
